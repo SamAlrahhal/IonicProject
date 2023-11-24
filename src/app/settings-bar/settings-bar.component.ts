d9fb8b1cfbdd1293bea617b8acb1fb1194127394
Sam Al-Rahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-11-24 18:20:59 +0100

    Update width input and save button functionality

--- src/app/settings-bar/settings-bar.component.ts
+++ src/app/settings-bar/settings-bar.component.ts
@@ -7,7 +7,14 @@ import { Renderer2, ElementRef } from '@angular/core';
   styleUrls: ['./settings-bar.component.scss'],
 })
 export class SettingsBarComponent implements OnInit {
+  inputValue?: string;
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit() {}
+
+  // This function is called when the user clicks the "Save" button
+  changeWidth() {
+    const button = this.el.nativeElement.querySelector('ion-button');
+    this.renderer.setStyle(button, 'width', this.inputValue + 'px');
+  }
 }
