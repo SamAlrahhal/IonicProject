32084bfd94745b5671ebe0bfcdef7722018d5fcd
Sam Al-Rahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-11-24 18:05:26 +0100

    Update navigation and settings buttons

--- src/app/settings-bar/settings-bar.component.ts
+++ src/app/settings-bar/settings-bar.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-settings-bar',
   templateUrl: './settings-bar.component.html',
   styleUrls: ['./settings-bar.component.scss'],
 })
-export class SettingsBarComponent  implements OnInit {
-
-  constructor() { }
+export class SettingsBarComponent implements OnInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit() {}
-
 }
