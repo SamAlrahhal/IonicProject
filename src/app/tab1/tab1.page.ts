4d809532df5e29a43e57c467569e8b9bc04cfdf0
Sam Al-Rahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-12-03 12:32:06 +0100

    it  kinda works

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  reply = '';
+  electronVersion = '';
+  constructor(private ngZone: NgZone) {
+    this.electronVersion = window.api.getElectronVersion();
+    window.api.ipcSendToMain();
+    window.api.ipcReceiveReplyFromMain(
+      'do-a-thing-reply',
+      (event: any, arg: any) => {
+        console.log(arg);
+        //Because the event handlers are called outside the context of Angular
+        //Angular Can't do "change detection" and the html won't be updated if the variables
+        //are changed. (Here reply via string interpolation in the html)
+        //Use the 2 lines below to show that the value has been changed but without the
+        //ngZone.run the new content will not be visible in the html
+        //this.reply = arg;
+        //console.log(this.reply);
+        this.ngZone.run(() => {
+          this.reply = arg;
+        });
+      }
+    );
+  }
 }
